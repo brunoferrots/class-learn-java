0ea70a9f6dcbbc2231996626bfc4ec8c3eb60e4d
brunoferrots <brunoferrots@outlook.com>
2023-03-10 11:46:10 -0300

    Testing arguments in command line

---
+++ src/learn/dio/basic/Arguments.java
@@ -0,0 +1,11 @@
+package learn.dio.basic;
+
+public class Arguments {
+    public static void main(String[] args) {
+        String name = args[0];
+        String lastName = args[1];
+        System.out.println("My name is " + name + " and last name " + lastName + "!");
+
+
+    }
+}
